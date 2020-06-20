34935f69200910af0076ee3cdf22aaef5197ad9d
jhgalvin <60234353+jhgalvin@users.noreply.github.com>
2020-06-19 21:18:18 -0500

    Add files via upload

--- PreDraft/profileCreate.php
+++ PreDraft/profileCreate.php
@@ -22,7 +22,7 @@ $user = $db->single();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Profile</title>
-    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="style.css"> <!-- it is fixed to style.css -->
     <link rel="stylesheet" href="sidebar.css">
 </head>
 
@@ -48,19 +48,71 @@ $user = $db->single();
         <form  id="submit" action="profileCreateScript.php" method="POST">
 
             <label for="companyName">Company Name:</label><br>
-			<input type="text"  name="companyName" required><br>
+			<input type="text"  name="companyName" required><br><br>
 
 			<label for="companyAddress1">Address 1:</label><br>
-			<input type="text" name="companyAddress1" required><br>
+			<input type="text" name="companyAddress1" required><br><br>
 			
 			<label for="companyAddress2">Address 2:</label><br>
-			<input type="text" name="companyAddress2"><br>
+			<input type="text" name="companyAddress2"><br><br>
 
             <label for="companyCity">City:</label><br>
-            <input  name = "companyCity" required><br>
+            <input type = "text" name = "companyCity" required><br><br>
 			
-			<label for="companyState">State:</label><br>
-            <input  name = "companyCity" required><br>
+			<label for = "companyState">Select State:</label><br>
+				<select id = "stateList" required>
+					<option value = "AL">Alabama</option>
+					<option value = "AK">Alaska</option>
+					<option value = "AZ">Arizona</option>
+					<option value = "AR">Arkansas</option>
+					<option value = "CA">California</option>
+					<option value = "CO">Colorado</option>
+					<option value = "CT">Connecticut</option>
+					<option value = "DE">Deleware</option>
+					<option value = "FL">Florida</option>
+					<option value = "GA">Georgia</option>
+					<option value = "HI">Hawaii</option>
+					<option value = "ID">Idaho</option>
+					<option value = "IL">Illinois</option>
+					<option value = "IN">Indiana</option>
+					<option value = "IA">Iowa</option>
+					<option value = "KS">Kansas</option>
+					<option value = "KY">Kentucky</option>
+					<option value = "LA">Louisiana</option>
+					<option value = "ME">Maine</option>
+					<option value = "MD">Maryland</option>
+					<option value = "MA">Massachussetts</option>
+					<option value = "MI">Michigan</option>
+					<option value = "MN">Minnesota</option>
+					<option value = "MS">Mississippi</option>
+					<option value = "MO">Missouri</option>
+					<option value = "MT">Montana</option>
+					<option value = "NE">Nebraska</option>
+					<option value = "NV">Nevada</option>
+					<option value = "NH">New Hampshire</option>
+					<option value = "NJ">New Jersey</option>
+					<option value = "NM">New Mexico</option>
+					<option value = "NY">New York</option>
+					<option value = "NC">North Carolina</option>
+					<option value = "ND">North Dakota</option>
+					<option value = "OH">Ohio</option>
+					<option value = "OK">Oklahoma</option>
+					<option value = "OR">Oregon</option>
+					<option value = "PA">Pennsylvania</option>
+					<option value = "RI">Rhode Island</option>
+					<option value = "SC">South Carolina</option>
+					<option value = "SD">South Dakota</option>
+					<option value = "TN">Tennessee</option>
+					<option value = "TX">Texas</option>
+					<option value = "UT">Utah</option>
+					<option value = "VT">Vermont</option>
+					<option value = "VA">Virginia</option>
+					<option value = "WA">Washington</option>
+					<option value = "WV">West Virginia</option>
+					<option value = "WI">Wisconsin</option>
+					<option value = "WY">Wyoming</option>
+				</select>
+			<br><br>
 
             <label for="companyZipCode">Zip Code:</label><br>
             <input type="text" name="companyZipCode" required><br>
