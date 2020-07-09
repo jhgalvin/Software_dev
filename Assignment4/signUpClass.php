73b39e8657930eb0f1bbe96e2932eb13e53258a2
IstvanW <55334150+IstvanW@users.noreply.github.com>
2020-07-09 15:34:23 -0500

    Add files via upload

--- Assignment4/signUpClass.php
+++ Assignment4/signUpClass.php
@@ -39,6 +39,8 @@ Class signUpClass
 		if($_SERVER['REQUEST_METHOD']=='POST')
 		{
 		
+			try
+		{
 			$sql = "INSERT INTO logincredentials (company_User, Company_Pass ) VALUES ('$this->company_User', '$this->company_Pass')";
 			$this->db->query($sql);
 			$this->result = $this->db->execute();
@@ -47,6 +49,13 @@ Class signUpClass
            		window.location.replace("login.php");
             		</script>'
 			;
+		}catch(PDOException $e){
+			echo '<script type="text/JavaScript">
+                	alert("An error occured (Username already exist).");
+               		window.location.replace("SignUp.php");
+                	</script>'
+			;
+		}
 		
 		}
 	}
